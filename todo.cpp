bbffe2baf5cc814aa1c663a1e5d4942b3f7aa88f
ksgr5566 <ksgr5566@gmail.com>
2020-12-18 00:17:07 +0530

    Update todo.cpp

--- todo.cpp
+++ todo.cpp
@@ -37,13 +37,22 @@ void report(string cwd){
     string line;
     if(todo.is_open()) while(getline(todo,line)) ++i;
     todo.close();
-    string line2;
-    done.open(directory+"done.txt",ios::in);
-    if(done.is_open()) while(getline(done,line2)) ++j;
     const int MAXLEN = 80;
     char s[MAXLEN];
     time_t t = time(0);
     strftime(s, MAXLEN, "%d/%m/%Y", localtime(&t));
+    string line2;
+    done.open(directory+"done.txt",ios::in);
+    while (std::getline(done, line2)){
+       int abc=0;
+       bool check = true;
+       for(auto z: s){
+           if(z!=line2[abc]) check=false;
+           ++abc;
+           if(abc>9) break;
+        }
+           if(check==true) ++j;
+    }
     cout << s << " Pending . " << i << " Completed . " << j << endl;
 }
 
@@ -52,6 +61,24 @@ void del(int i,string cwd){
     directory.erase(directory.find_last_of('\\')+1);
     string file1= directory+"todo.txt";
     string file2= directory+"temp.txt";
+    fstream todo;
+    todo.open(file1,ios::in);
+    int lines_in_todo= 0;
+    string line_in_todo;
+    if(todo.is_open()) while(getline(todo,line_in_todo)) ++lines_in_todo;
+    todo.close();
+    if(lines_in_todo==0){
+        cout << "No Tasks / Error opening the file" << endl;
+        return;
+    }
+    if(i>lines_in_todo){
+        cout << "Error: todo #" << i <<" does not exist." << endl;
+        return;
+    }
+    if(i<=0){
+        cout << "Error: invalid task number" << endl;
+        return;
+    }
     ifstream is(file1);
     ofstream temp_file;
     temp_file.open(file2, ofstream::out);
@@ -93,29 +120,55 @@ void done(int i,string cwd){
     directory.erase(directory.find_last_of('\\')+1);
     string file1= directory+"todo.txt";
     string file2= directory+"temp.txt";
-    ifstream is(directory+"todo.txt");
+    fstream todo;
+    todo.open(file1,ios::in);
+    int lines_in_todo= 0;
+    string line_in_todo;
+    if(todo.is_open()) while(getline(todo,line_in_todo)) ++lines_in_todo;
+    todo.close();
+    if(lines_in_todo==0){
+        cout << "No Tasks / Error opening the file" << endl;
+        return;
+    }
+    if(i>lines_in_todo){
+        cout << "Error: todo #" << i <<" does not exist." << endl;
+        return;
+    }
+    if(i<=0){
+        cout << "Error: invalid task number" << endl;
+        return;
+    }
+    ifstream is(file1);
     ofstream temp_file;
-    temp_file.open(directory+"temp.txt", ofstream::out);
+    temp_file.open(file2, ofstream::out);
     fstream done;
-    done.open(directory+"done.txt",ios::in | ios::app );
+    done.open(directory+"done.txt",ios::in);
     const int MAXLEN = 80;
     char s[MAXLEN];
     time_t t = time(0);
     strftime(s, MAXLEN, "%d/%m/%Y", localtime(&t));
+    int lines=0;
+    string line;
+    if(done.is_open()) while(getline(done,line)) ++lines;
+    done.close();
+    done.open(directory+"done.txt",ios::app );
     char c;
     if(i!=1){
         int line_no = 1;
+        done << s << "  " ;
         while (is.get(c)){
             if (c == '\n') line_no++;
             if (line_no != i) temp_file << c;
-            else done << s << "  "  << c;
+            else if(c!='\n') done  << c;
         }
+        done << '\n' ; 
     }else{
-         done << '\n';
+         done <<  s << "  " ;
          while (is.get(c)){
              if(c=='\n') break;
-             done << s << "  "  << c;
+             done << c;
          }
+         done << '\n';
          while (is.get(c)) temp_file << c;
     }
     done.close();
